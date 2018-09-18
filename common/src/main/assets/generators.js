cdd1c0c328c4a8da5cb50fef2ec4f901ce630ee4
Raul Portales <rportales@oneviewhealthcare.com>
2018-09-18 09:59:36 +0100

    fixed generator that takes a list

--- common/src/main/assets/generators.js
+++ common/src/main/assets/generators.js
@@ -104,7 +104,7 @@ Blockly.JavaScript['rainbow_hat_write_led_strip_array'] = function(block) {
     var colorsArray = Blockly.JavaScript.valueToCode(block, 'COLORS', Blockly.JavaScript.ORDER_FUNCTION_CALL) || "[]";
     return  'var colors = [];\n'+
             'for (var i = 0; i < '+colorsArray+'.length; i++) {\n'+
-            '   colors[i] = '+colorsArray+'[i].replace(\"#\",\"0x\");\n'+
+            '   colors[i] = parseInt('+colorsArray+'[i].replace(\"#\",\"0x\"));\n'+
             '}\n'+
             'Apa102.write(colors);';
 };
