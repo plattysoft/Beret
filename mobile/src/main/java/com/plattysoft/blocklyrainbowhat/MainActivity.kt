b9be61fd9c1f57210aa1a225fcb18b4c47c28cb0
Raul Portales <rportales@oneviewhealthcare.com>
2018-09-18 09:42:02 +0100

    split into common library, create default mobile project

--- mobile/src/main/java/com/plattysoft/blocklyrainbowhat/MainActivity.kt
+++ mobile/src/main/java/com/plattysoft/blocklyrainbowhat/MainActivity.kt
@@ -1,17 +1,7 @@
 package com.plattysoft.blocklyrainbowhat
 
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import android.webkit.JavascriptInterface
-import android.webkit.WebView
-import com.google.android.things.contrib.driver.apa102.Apa102
-import com.google.android.things.contrib.driver.bmx280.Bmx280
-import com.google.android.things.contrib.driver.button.Button
-import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay
-import com.google.android.things.contrib.driver.pwmspeaker.Speaker
-import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
-import com.google.android.things.pio.Gpio
 import com.google.blockly.android.codegen.CodeGenerationRequest
 
 class MainActivity : RainbowHatBlocklyBaseActivity() {
