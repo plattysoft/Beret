bceb3abfc0f51b2f8cbdf1854aa9f3c796264053
plattysoft <raul@plattysoft.com>
2018-09-23 19:01:54 +0100

    improved flow for discovery / connecting / disconnected from mobile

--- mobile/src/main/java/com/plattysoft/blocklyrainbowhat/MainActivity.kt
+++ mobile/src/main/java/com/plattysoft/blocklyrainbowhat/MainActivity.kt
@@ -2,94 +2,25 @@ package com.plattysoft.blocklyrainbowhat
 
 import android.os.Bundle
 import android.util.Log
-import com.google.android.gms.nearby.Nearby
-import com.google.android.gms.nearby.connection.*
 import com.google.blockly.android.codegen.CodeGenerationRequest
 
 class MainActivity : RainbowHatBlocklyBaseActivity() {
 
-    companion object {
-        private const val NICKNAME = "Remote Editor" // This could be the device ID
-        private val TAG = MainActivity::class.java.simpleName
-    }
-
-    lateinit var nearbyConnection : ConnectionsClient
-    private var connectedEndpoint: String? = null
-
-    private val endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
-        override fun onEndpointFound(endpointId: String, discoveredEndpointInfo: DiscoveredEndpointInfo) {
-            connectToEndpoint(endpointId)
-        }
-        override fun onEndpointLost(endpointId: String) {
-            // A previously discovered endpoint has gone away.
-        }
-    }
-
-    private val connectionLifecycleCallback = object : ConnectionLifecycleCallback(){
-        override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
-            if (result.status.statusCode == ConnectionsStatusCodes.STATUS_OK) {
-                connectedEndpoint = endpointId
-            }
-        }
-        override fun onDisconnected(endpointId: String) {
-            if (endpointId.equals(connectedEndpoint)) {
-                connectedEndpoint = null
-            }
-        }
-        override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
-            nearbyConnection.acceptConnection(endpointId, payloadCallback)
-        }
-    }
-
-    private val payloadCallback = object: PayloadCallback() {
-        override fun onPayloadReceived(p0: String, payload: Payload) {
-            // Not expecting to receive anything yet
-        }
-
-        override fun onPayloadTransferUpdate(p0: String, p1: PayloadTransferUpdate) {
-            // Nothing to do here
-        }
-    }
+    lateinit private var nearbyWrapper: NearbyWrapper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        nearbyConnection = Nearby.getConnectionsClient(this)
-        startDiscovery()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        nearbyConnection.stopDiscovery()
-    }
-
-    private fun connectToEndpoint(endpointId: String) {
-        nearbyConnection.requestConnection(
-                NICKNAME,
-                endpointId,
-                connectionLifecycleCallback)
-    }
-
-    private fun startDiscovery() {
-        nearbyConnection.startDiscovery(
-                SERVICE_ID,
-                endpointDiscoveryCallback,
-                DiscoveryOptions.Builder().setStrategy(STRATEGY).build())
-                .addOnFailureListener {
-                    Log.e(TAG, "onFailure: "+it.localizedMessage)
-                }
-                .addOnSuccessListener {
-                    Log.e(TAG, "Discovering...")
-                }
+        nearbyWrapper = NearbyWrapper(this)
     }
 
     private fun loadProgram(program: String) {
+        nearbyWrapper.sendProgram(program)
         Log.d("Program", program)
     }
 
     override fun getCodeGenerationCallback(): CodeGenerationRequest.CodeGeneratorCallback {
         return CodeGenerationRequest.CodeGeneratorCallback {
             loadProgram(it)
-            nearbyConnection.sendPayload(connectedEndpoint!!, Payload.fromBytes(it.toByteArray()))
         }
     }
 }
---
+++ mobile/src/main/java/com/plattysoft/blocklyrainbowhat/NearbyWrapper.kt
@@ -0,0 +1,104 @@
+package com.plattysoft.blocklyrainbowhat
+
+import android.content.Context
+import android.util.Log
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.*
+import com.google.blockly.android.AbstractBlocklyActivity
+
+class NearbyWrapper (context: Context){
+
+    companion object {
+        private const val NICKNAME = "Remote Editor" // This could be the device ID
+        private val TAG = NearbyWrapper::class.java.simpleName
+    }
+
+    private val nearbyConnection = Nearby.getConnectionsClient(context)
+
+    private var currentProgram: String = ""
+
+    private var connectedEndpoint: String? = null
+
+    private val endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
+        override fun onEndpointFound(endpointId: String, discoveredEndpointInfo: DiscoveredEndpointInfo) {
+            Log.e(TAG, "onEndpointFound: start: "+endpointId)
+            connectToEndpoint(endpointId)
+            nearbyConnection.stopDiscovery()
+        }
+        override fun onEndpointLost(endpointId: String) {
+            // A previously discovered endpoint has gone away.
+        }
+    }
+
+    private val connectionLifecycleCallback = object : ConnectionLifecycleCallback(){
+        override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+            if (result.status.statusCode == ConnectionsStatusCodes.STATUS_OK) {
+                Log.e(TAG, "onConnectionResult: STATUS_OK")
+                connectedEndpoint = endpointId
+                Log.e(TAG, "sendPayload: start")
+                nearbyConnection.sendPayload(connectedEndpoint!!, Payload.fromBytes(currentProgram.toByteArray()))
+            }
+        }
+        override fun onDisconnected(endpointId: String) {
+            if (endpointId.equals(connectedEndpoint)) {
+                connectedEndpoint = null
+            }
+        }
+        override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
+            nearbyConnection.acceptConnection(endpointId, payloadCallback)
+        }
+    }
+
+    private val payloadCallback = object: PayloadCallback() {
+        override fun onPayloadReceived(p0: String, payload: Payload) {
+            // Not expecting to receive anything yet
+        }
+
+        override fun onPayloadTransferUpdate(p0: String, p1: PayloadTransferUpdate) {
+            // When we are done sending the payload, we disconnect and stop discovering
+            if (p1.status == PayloadTransferUpdate.Status.SUCCESS) {
+                Log.e(TAG, "onPayloadTransferUpdate: SUCCESS")
+
+                if (connectedEndpoint != null) {
+                    nearbyConnection.disconnectFromEndpoint(connectedEndpoint!!)
+                }
+                connectedEndpoint = null
+            }
+        }
+    }
+
+    private fun connectToEndpoint(endpointId: String) {
+        Log.e(TAG, "connectToEndpoint: start")
+
+        nearbyConnection.requestConnection(
+                NICKNAME,
+                endpointId,
+                connectionLifecycleCallback)
+    }
+
+    private fun startDiscovery() {
+        Log.e(TAG, "startDiscovery: start")
+
+        nearbyConnection.startDiscovery(
+                RainbowHatBlocklyBaseActivity.SERVICE_ID,
+                endpointDiscoveryCallback,
+                DiscoveryOptions.Builder().setStrategy(RainbowHatBlocklyBaseActivity.STRATEGY).build())
+                .addOnFailureListener {
+                    Log.e(TAG, "startDiscovery: onFailure: "+it.localizedMessage)
+                }
+                .addOnSuccessListener {
+                    Log.e(TAG, "startDiscovery: Discovering...")
+                }
+    }
+
+    fun sendProgram(program: String) {
+        Log.e(TAG, "sendProgram: start")
+
+        currentProgram = program
+        // We only send them if we are in a good state
+        if (connectedEndpoint != null) {
+            nearbyConnection.disconnectFromEndpoint(connectedEndpoint!!)
+        }
+        startDiscovery()
+    }
+}
