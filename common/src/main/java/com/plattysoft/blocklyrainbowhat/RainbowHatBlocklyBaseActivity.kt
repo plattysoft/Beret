5638c12fb138c0c9f5e368b30ba19aaca1a6f938
plattysoft <raul@plattysoft.com>
2018-09-30 14:35:23 +0100

    #3 added confirmation to clean workspace

--- common/src/main/java/com/plattysoft/blocklyrainbowhat/RainbowHatBlocklyBaseActivity.kt
+++ common/src/main/java/com/plattysoft/blocklyrainbowhat/RainbowHatBlocklyBaseActivity.kt
@@ -1,10 +1,12 @@
 package com.plattysoft.blocklyrainbowhat
 
-import com.google.android.gms.nearby.connection.Payload
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.view.MenuItem
 import com.google.android.gms.nearby.connection.Strategy
 import com.google.blockly.android.AbstractBlocklyActivity
 import com.google.blockly.model.DefaultBlocks
-import org.json.JSONObject
+import com.plattysoft.blocklyrainbowhat.common.R
 import java.util.*
 
 /**
@@ -14,7 +16,7 @@ abstract class RainbowHatBlocklyBaseActivity : AbstractBlocklyActivity() {
 
     companion object {
         const val SERVICE_ID = "com.plattysoft.nearby"
-        val STRATEGY = Strategy.P2P_STAR
+        val STRATEGY = Strategy.P2P_STAR as Strategy
     }
 
     override fun getToolboxContentsXmlPath(): String {
@@ -31,9 +33,46 @@ abstract class RainbowHatBlocklyBaseActivity : AbstractBlocklyActivity() {
     override fun getGeneratorsJsPaths(): MutableList<String> {
         return Arrays.asList("rainbowHat_generators.js", "events_generators.js")
     }
-}
 
-fun Payload.getAsJson(): JSONObject {
-    val string = String(this.asBytes()!!)
-    return JSONObject(string)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.action_clear) {
+            // Ask for confirmation
+            if (controller.workspace.hasBlocks()) {
+                askForClearConfirmation()
+            }
+            return true
+        }
+//        else if (id == R.id.action_save) {
+//            onSaveWorkspace()
+//            return true
+//        } else if (id == R.id.action_load) {
+//            onLoadWorkspace()
+//            return true
+//        }  else if (id == R.id.action_run) {
+//            if (controller.workspace.hasBlocks()) {
+//                onRunCode()
+//            } else {
+//                Log.i(TAG, "No blocks in workspace. Skipping run request.")
+//            }
+//            return true
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun askForClearConfirmation() {
+        // Consider doing this with a snackbar and an UNDO option instead
+        AlertDialog.Builder(this)
+                .setMessage(R.string.confirm_clear_workspace_message)
+                .setTitle(R.string.confirm_clear_workspace_title)
+                .setPositiveButton(R.string.yes, { dialogInterface: DialogInterface, i: Int ->
+                    onClearWorkspace()
+                    dialogInterface.dismiss()
+                })
+                .setNegativeButton(R.string.no, { dialogInterface: DialogInterface, i: Int ->
+                    dialogInterface.dismiss()
+                })
+                .create()
+                .show()
+    }
 }
